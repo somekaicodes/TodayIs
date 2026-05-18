3011ca59c92ef66ea6004df3c9484a4347f4e449
Kai Kim <kkmin123446@gmail.com>
2026-05-17 21:31:21 -0600

    Initial Commit

---
+++ TodayIs/TodayIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayIsApp.swift
+//  TodayIs
+//
+//  Created by Kai Kim on 2026-05-17.
+//
+
+import SwiftUI
+
+@main
+struct TodayIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
