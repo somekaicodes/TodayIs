1e1f4499d229063b0c0dedd327a9b2a576b97852
Kai Kim <kyuminkim7979@gmail.com>
2026-05-17 22:55:02 -0600

    Add RootView shell

---
+++ TodayIs/Views/RootView.swift
@@ -0,0 +1,14 @@
+//
+//  RootView.swift
+//  TodayIs
+//
+//  Created by Kai Kim on 2026-05-17.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
